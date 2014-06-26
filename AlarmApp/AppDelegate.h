3d37a565400b8bcab1db17882909fe585a7beff0
Kevin Kwon <myhhkb@gmail.com>
2014-06-27 01:12:53 +0900

    Initial Commit

---
+++ AlarmApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AlarmApp
+//
+//  Created by hdk on 2014. 6. 27..
+//  Copyright (c) 2014년 Kevin Kwon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AlarmAppTests/AlarmAppTests.m
@@ -0,0 +1,34 @@
+//
+//  AlarmAppTests.m
+//  AlarmAppTests
+//
+//  Created by hdk on 2014. 6. 27..
+//  Copyright (c) 2014년 Kevin Kwon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AlarmAppTests : XCTestCase
+
+@end
+
+@implementation AlarmAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
